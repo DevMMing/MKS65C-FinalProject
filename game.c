533df5e48cca23fb27a4237f466d363a19cceff1
DevMMing <mming@stuy.edu>
2019-01-08 15:36:09 -0500

    to be or not to be

--- game.c
+++ game.c
@@ -2,7 +2,23 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <time.h>
-
-int main(){
+#define playerCount 2
+typedef struct unit{// stats 
+  int attk;
+  int dfns;
+  int intll;
+  int dxt;
+  int hp;
+  char * name;
+} monster;player
+int game(){
+  
+}
+int behavior(unit character){
+  while(1){
+  
+  }
+}
+int attk(unit attker,unit dfnder){
   
 }
